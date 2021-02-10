9c38d3f7e6e9c947ca77a186f979ddc15f855429
Nikolay Lazarov <nikolay.lazarov@uber.com>
2021-02-10 19:28:35 +0200

    Add some buttons

--- bots/beer-bot/bot.py
+++ bots/beer-bot/bot.py
@@ -11,7 +11,58 @@ app = App(
 @app.message("hello")
 def message_hello(message, say):
     # say() sends a message to the channel where the event was triggered
-    say(f"Hey there <@{message['user']}>!")
+    say(
+        blocks=[
+            {
+                "type": "section",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Да треперят",
+                    "emoji": True
+                }
+            },
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Да",
+                            "emoji": True
+                        },
+                        "value": "yes",
+                        "action_id": "yes"
+                    },
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Не",
+                            "emoji": True
+                        },
+                        "value": "no",
+                        "action_id": "no"
+                    }
+                ]
+            }
+        ],
+        text=f"Hey there <@{message['user']}>!"
+    )
+
+
+@app.action("yes")
+def yes(body, ack, say):
+    # Acknowledge the action
+    ack()
+    say(f"<@{body['user']['id']}> clicked yes")
+
+
+@app.action("no")
+def yes(body, ack, say):
+    # Acknowledge the action
+    ack()
+    say(f"<@{body['user']['id']}> clicked no")
 
 # Start your app
 if __name__ == "__main__":

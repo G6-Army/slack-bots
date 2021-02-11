cc1168f77e1b68b31a098e0fff91e25a3f9cebcd
Nikolay Lazarov <nikolay.lazarov@uber.com>
2021-02-11 22:35:07 +0200

    Implement very simple file database

--- bots/beer-bot/db.py
+++ bots/beer-bot/db.py
@@ -1,3 +1,21 @@
+import json
+
+def _read_data():
+    with open('database.json', 'w+') as f:
+        data = f.read()
+        if data == '':
+            return {}
+        return json.loads(data)
+
+
+def _write_data(data):
+    with open('database.json', 'w') as f:
+        f.writelines([json.dumps(data)])
+
+
 def upsert_user_salary_date(user, day):
-    # TODO
-    pass
+    data = _read_data()
+    if user not in data:
+        data[user] = {}
+    data[user]['salary_day'] = day
+    _write_data(data)
